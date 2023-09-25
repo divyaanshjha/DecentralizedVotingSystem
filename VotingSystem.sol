57ed7b9b5600d9264b3a0a01000f56d55e2d1b0f
divyaanshjha <135268811+divyaanshjha@users.noreply.github.com>
2023-09-26 05:27:30 +0530

    Update VotingSystem.sol

--- VotingSystem.sol
+++ VotingSystem.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VotingToken is ERC20("VoteToken", "VTO"), Ownable {
-    function mint() public onlyOwner {
-        _mint(msg.sender, 10**25);    }
+//    function mint() public onlyOwner {
+//      _mint(msg.sender, 10**25);    }
     
     struct Candidate {
         string name;
